89b18e137e9fc8bdf9cf4e1d4d6c90874a5d575d
JALE <jalemedlin@gmail.com>
2022-04-18 16:39:04 +0300

    Job offer is done

--- src/EuroBacth/FutureSdet.java
+++ src/EuroBacth/FutureSdet.java
@@ -3,5 +3,6 @@ package EuroBacth;
 public class FutureSdet {
     public static void main(String[] args) {
         System.out.println("Annually pay is $125.528");
+        System.out.println("Job is ready for you and offered");
     }
 }
